5e70c43bfd85043d9a39ddbe674a887b192b6a27
Jonas Holbech <jofh@kea.dk>
2020-02-18 10:24:21 +0100

    add logtodom and similar methods to FLIP, allow chaining

--- FLIP/FLIP.js
+++ FLIP/FLIP.js
@@ -1,23 +1,53 @@
 export default class FLIP {
-  constructor(el) {
+  constructor(el, scale = true) {
     this.el = el;
     this.first = null;
     this.last = null;
+    this.scale = scale;
   }
-  logInitialPosition() {
+  logFirstPosition() {
     this.first = this.el.getBoundingClientRect();
+    return this;
+  }
+  logFirstToDOM() {
+    this.el.dataset.first = JSON.stringify(this.first);
+    return this;
+  }
+  readFirstFromDOM() {
+    this.first = JSON.parse(this.el.dataset.first);
+    return this;
   }
   logFinalPosition() {
     this.last = this.el.getBoundingClientRect();
+    return this;
   }
-  animate(duration) {
+  logLastToDOM() {
+    this.el.dataset.last = JSON.stringify(this.last);
+    return this;
+  }
+  readLastFromDOM() {
+    this.last = JSON.parse(this.el.dataset.last);
+    return this;
+  }
+  clearDOM() {
+    this.el.removeAttribute("data-first");
+    this.el.removeAttribute("data-last");
+    return this;
+  }
+  animate(duration = 600, callback = null) {
     const deltaX = this.first.left - this.last.left;
     const deltaY = this.first.top - this.last.top;
-    this.el.animate(
+    const deltaW = this.first.width / this.last.width;
+    const deltaH = this.first.height / this.last.height;
+    let transform = `translate(${deltaX}px, ${deltaY}px) scale(${deltaW}, ${deltaH})`;
+    if (!this.scale) {
+      transform = `translate(${deltaX}px, ${deltaY}px)`;
+    }
+    const x = this.el.animate(
       [
         {
           transformOrigin: "top left",
-          transform: `translate(${deltaX}px, ${deltaY}px)`
+          transform
         },
         {
           transformOrigin: "top left",
@@ -30,5 +60,7 @@ export default class FLIP {
         fill: "both"
       }
     );
+    x.onfinish = callback;
+    return this;
   }
 }
--- FLIP/README.md
+++ FLIP/README.md
@@ -1,13 +1,61 @@
 # usage
 
+## Basic usage
+
 ```js
 import FLIP from "./FLIP.js";
 
 const el = dom.deck.querySelector("div.flip-card:last-child");
 el.style.zIndex = 100;
 const flip = new FLIP(el);
-flip.logInitialPosition();
+flip.logFirstPosition();
 somewhere.appendChild(el);
 flip.logFinalPosition();
 flip.animate(600);
+```
+
+## Methods and arguments
+
+### Basic setup
+
+```js
+import FLIP from "./FLIP.js";
+```
+
+### `constructor`
+
+```js
+const flip = new FLIP(el, (scale = true));
+```
+
+If `scale` is true (default) the FLIP animation will use transform: scale.. to animate size changes
+
+### `.logFirstPosition`, `.logLastPosition`
+
+Stores the position of the element before and after transformations
+
+### `logFirstToDOM`, `logLastToDOM`
+
+Logs the position of the element (before or after) transformations to a data-attribute on the `el`
+
+### `readFirstFromDOM`, `readLastFromDOM`
+
+Reads data store with `logFirstToDOM`, `logLastToDOM` and sets the first and/or last position from the data-attribute
+
+### `.clearDOM`
+
+Removes data-attributes set with `logFirstToDOM`, `logLastToDOM`.
+
+### `.animate(duration = 600, callback = null)`
+
+Sets the duration of the animation and optionally a callback to be executed when the animation finishes
+
+## Chaining
+
+All methods can be chained (but since `.animate` has a duration, that one could trip you up).
+
+### Example
+
+```js
+const flip = new FLIP(el).logFirstPosition().logFirstToDOM(); //...
 ```
